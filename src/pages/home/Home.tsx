ec1c2e57e3357495f04e419af52debb3d75d9584
bilgejava6 <bilgejava6@gmail.com>
2024-07-17 13:57:57 +0300

    home page end

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -281,139 +281,28 @@ function Home() {
                         </div>
                                                
                         <div className="card-body">
-
                             <div className="media">
                                 <img src="/img/avatar-dhg.png" alt="img" width="55px" height="55px" className="rounded-circle mr-3" />
-
                                 <div className="media-body">
                                     <h5>Dave Gamache</h5>
                                     <p className="card-text text-justify">Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-                                
-
-                                    <div className="row no-gutters mb-3">
-                                        <div className="col-6 p-1 text-center">
-
-                                                <img src="/img/adventure-alps-clouds-2259810.jpg" alt="" className="img-fluid mb-2"/>
-                                                <img src="/img/aerial-view-architectural-design-buildings-2228123.jpg" alt="" className="img-fluid" />
-                                    
-
-                                        </div>
-
-                                        <div className="col-6 p-1 text-center">
-
-                                                <img src="/img/celebration-colored-smoke-dark-2297472.jpg" alt="" className="img-fluid mb-2" />
-                                                <img src="/img/bodybuilding-exercise-fitness-2294361.jpg" alt=""className="img-fluid" />
-                                    
-
-                                        </div>
-
-
+                                    <div className="col-12 p-1 text-center">
+                                                <img src="/img/mid5.jpg" alt="" className="img-fluid shadow-sm img-thumbnail"/>
                                     </div>
-
-
-
-
-
-
-
-
-                                
+                                    
+                                      <small>5min</small>
                                     <div className="media mb-3">
                                             <img src="/img/avatar-dhg.png" alt="img" width="45px" height="45px" className="rounded-circle mr-2" />
                                             <div className="media-body">
                                                     <p className="card-text text-justify">Jacon Thornton: Donec id elit non mi porta gravida at eget metus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis.</p>
 
                                             </div>
-
-
                                     </div>
-
-                                    
-                                    <div className="media">
-                                            <img src="/img/avatar-mdo.png" alt="img" width="45px" height="45px" className="rounded-circle mr-2"/>
-                                            <div className="media-body">
-                                                    <p className="card-text text-justify">Mark Otto: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-
-                                            </div>
-
-
-                                    </div>
-
-                                    
-                                    
-
-
-
-                                </div>
-                                <small>5min</small>
-                                
-
-
+                                </div>                            
                             </div>
-
-
-                            
-
-
-
-                        </div>
-                                  
+                        </div>                                  
                         <hr />
-
-                        <div className="card-body">
-
-                            <div className="media">
-                                    <img src="/img/avatar-fat.jpg" alt="img" width="55px" height="55px" className="rounded-circle mr-3"/>
-
-                                <div className="media-body">
-                                        <h5>Jacob Thornton</h5>
-                                        <p className="text-justify">Donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-                                            Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-
-
-                                </div>
-
-                                <small>14 min</small>
-
-                            </div>
-
-
-
-
-
-                        </div>
-                        <hr />
-
-                        <div className="card-body">
-
-                            <div className="media">
-
-                                    <img src="/img/avatar-mdo.png" alt="img" width="55px" height="55px" className="rounded-circle mr-3"/>
-                                    <div className="media-body">
-                                        <h5>Mark Otto</h5>
-                                        <p className="text-justify">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                                    <a href="/img/mid5.jpg" data-lightbox="id"><img src="/img/mid5.jpg" alt="" className="img-fluid shadow-sm img-thumbnail"/>   </a>   
-
-                                    </div>
-
-
-                                    <small className="text-muted">10 min</small>
-
-
-
-                            </div>
-
-
-
-
-                        </div>
-
-
                     </div>
-
-
                 </div>
 
             </div>
@@ -425,31 +314,16 @@ function Home() {
 
                 <div className="right-column">
 
-                    <div className="card shadow-sm mb-4" >
-                        <div className="card-body">
-                            <h6 className="card-title">Sponsored</h6>
-                            <img src="/img/right1.jpg" alt="card-img" className="card-img mb-3"/>
-                            <p className="card-text text-justify"> <span className="h6">It might be time to visit Iceland.</span> Iceland is so chill, and everything looks cool here. Also, we heard the people are pretty nice.  What are you waiting for?</p>
-                            <a href="#" className="btn btn-outline-info card-link btn-sm">Buy a ticket</a>
-
-
-                        </div>
-
-                    </div>
-
-
+                   
                     <div className="card shadow-sm mb-4">
 
                         <div className="card-body">
 
-                                <h6 className="card-title ">Likes <a href="#" className="ml-1"><small>.View All</small> </a> </h6>
+                                <h6 className="card-title ">Likes <a href="#" className="ml-1"><small>View All</small> </a> </h6>
                                 <div className="row no-gutters d-none d-lg-flex">
                                     <div className="col-6 p-1">
                                             <img src="/img/avatar-dhg.png" alt="img" width="80px" height="80px" className="rounded-circle mb-4"/>
                                             <img src="/img/avatar-fat.jpg" alt="img" width="80px" height="80px" className="rounded-circle"/>
-
-
-
                                     </div>
                                     <div className="col-6 p-1 text-left">
                                         <h6>Jacob Thornton @fat</h6>
@@ -469,49 +343,12 @@ function Home() {
                             <p className="lead" style={{fontSize: '18px'}}>Dave really likes these nerds, no one knows why though.</p>
                         </div>
                         
-
-
                     </div>
 
                     <div className="card shadow-sm">
                         <div className="card-body">
-                            <p>&copy; 2018 Bootstrap
-
-
-
-                                    <a href="#">About</a>
-                                    <a href="#">Help</a>
-                                    <a href="#">Terms</a>
-                                    <a href="#">Privacy</a>
-                                    <a href="#">Cookies</a>
-                                    <a href="#">Ads </a>
-                                    <a href="#">Info</a>
-                                    <a href="#">Brand</a>
-                                    <a href="#">Blog</a>
-                                    <a href="#">Status</a>
-                                    <a href="#">Apps</a>
-                                    <a href="#">Jobs</a>
-                                    <a href="#">Advertise</a>
-                                
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            </p>
+                            <p>&copy; 2024 Java Boost 14 </p>
                         </div>
-
                     </div>
 
 
@@ -523,33 +360,6 @@ function Home() {
 
 
                 </div>
-
-                
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
             </div>
 
